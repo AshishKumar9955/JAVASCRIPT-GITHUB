3a7edfacf5e7ef0147f72585d69934036ca73587
AshishKumar9955 <ashishrajltr2003@gmail.com>
2023-12-08 10:14:56 +0530

    add topic stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -27,3 +27,26 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+// *******************************************************
+// Stack (Primitive), Heap (Non-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
